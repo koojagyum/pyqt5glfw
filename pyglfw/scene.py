83a8d2ff124ddd51b7a4fe29d7656a27ece8a9fc
Jagyum Koo <koojagyum@gmail.com>
2018-10-18 08:27:51 +0900

    pyglfw: Instance parser

--- pyglfw/scene.py
+++ pyglfw/scene.py
@@ -4,6 +4,8 @@ import os
 
 from .camera import Camera
 from .model import load_model
+from .model import InstanceRenderer
+from .model import ModelInstance
 from OpenGL.GL import *
 from .renderer import *
 
@@ -36,6 +38,43 @@ def load_resources(resources_dic, basepath='.'):
     return resource_list
 
 
+def load_instance(instances_dic, model_list):
+    def _pick(dic, key):
+        if key not in dic:
+            return None
+        return dic[key]
+
+    instance_list = {}
+
+    for i in instances_dic:
+        name = _pick(i, 'name')
+        model = None
+        model_name = _pick(i, 'model')
+        if model_name in model_list:
+            model = model_list[model_name]
+        # renderer_name = _pick(i, 'renderer')
+        # renderer = globals()[renderer_name]
+        # print(renderer)
+        translation = _pick(i, 'translation')
+        rotation = _pick(i, 'rotation')
+        scale = _pick(i, 'scale')
+
+        instance = ModelInstance(
+            name=name,
+            model=model,
+            translation=translation,
+            rotation=rotation,
+            scale=scale
+        )
+        instance_list[name] = instance
+
+    return instance_list
+
+
+def load_camera(camera_dic):
+    return None
+
+
 def load_fromjson(jsonpath):
     with open(jsonpath) as f:
         desc = json.load(f)
@@ -62,6 +101,11 @@ def load_fromjson(jsonpath):
     resource_list = load_resources(resources, basepath)
     print('resources:')
     print(resource_list)
+    print('')
+
+    instance_list = load_instance(instances, resource_list['model'])
+    print('instances:')
+    print(instance_list)
     print('')
 
 
