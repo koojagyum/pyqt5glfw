57eed5a55c03ee9f324d10dc1e0cd13e324a2048
Jagyum Koo <koojagyum@gmail.com>
2018-10-21 17:06:26 +0900

    pyglfw: Add PointLight & example

--- pyglfw/light.py
+++ pyglfw/light.py
@@ -2,9 +2,9 @@ import numpy as np
 
 
 def load_light(desc):
-    def _pick(dic, key):
+    def _pick(dic, key, default=None):
         if key not in dic:
-            return None
+            return default
         return dic[key]
 
     def _pick_nparray(dic, key, dtype=np.float32):
@@ -28,22 +28,41 @@ def load_light(desc):
             diffuse=diffuse,
             specular=specular
         )
+    elif class_name == 'PointLight':
+        print(desc)
+        position = _pick_nparray(desc, 'position')
+        ambient = _pick_nparray(desc, 'ambient')
+        diffuse = _pick_nparray(desc, 'diffuse')
+        specular = _pick_nparray(desc, 'specular')
+
+        constant = _pick(desc, 'constant', 0.0)
+        linear = _pick(desc, 'linear', 0.0)
+        quadratic = _pick(desc, 'quadratic', 0.0)
+
+        return PointLight(
+            name=name,
+            position=position,
+            ambient=ambient,
+            diffuse=diffuse,
+            specular=specular,
+            constant=constant,
+            linear=linear,
+            quadratic=quadratic
+        )
 
     return None
 
 
-class DirectionalLight:
+class Light:
 
     def __init__(self,
-                 name='dirLight',
-                 direction=None,
+                 name='light',
                  ambient=None,
                  diffuse=None,
                  specular=None):
         zeros = np.zeros((3), dtype=np.float32)
         ones = np.ones((3), dtype=np.float32)
 
-        self.direction = (direction, zeros)[direction is None]
         self.ambient = (ambient, zeros)[ambient is None]
         self.diffuse = (diffuse, ones)[diffuse is None]
         self.specular = (specular, zeros)[specular is None]
@@ -51,7 +70,63 @@ class DirectionalLight:
         self.name = name
 
     def update(self, program):
-        program.setVec3f(self.name + '.direction', self.direction)
         program.setVec3f(self.name + '.ambient', self.ambient)
         program.setVec3f(self.name + '.diffuse', self.diffuse)
         program.setVec3f(self.name + '.specular', self.specular)
+
+
+class DirectionalLight(Light):
+
+    def __init__(self,
+                 name='dirLight',
+                 direction=None,
+                 ambient=None,
+                 diffuse=None,
+                 specular=None):
+        super().__init__(
+            name=name,
+            ambient=ambient,
+            diffuse=diffuse,
+            specular=specular
+        )
+
+        zeros = np.zeros((3), dtype=np.float32)
+        self.direction = (direction, zeros)[direction is None]
+
+    def update(self, program):
+        super().update(program)
+        program.setVec3f(self.name + '.direction', self.direction)
+
+
+class PointLight(Light):
+
+    def __init__(self,
+                 name='pointLight',
+                 position=None,
+                 ambient=None,
+                 diffuse=None,
+                 specular=None,
+                 constant=0.0,
+                 linear=0.0,
+                 quadratic=0.0):
+        super().__init__(
+            name=name,
+            ambient=ambient,
+            diffuse=diffuse,
+            specular=specular
+        )
+
+        zeros = np.zeros((3), dtype=np.float32)
+        self.position = (position, zeros)[position is None]
+
+        self.constant = constant
+        self.linear = linear
+        self.quadratic = quadratic
+
+    def update(self, program):
+        super().update(program)
+
+        program.setVec3f(self.name + '.position', self.position)
+        program.setFloat(self.name + '.constant', self.constant)
+        program.setFloat(self.name + '.linear', self.linear)
+        program.setFloat(self.name + '.quadratic', self.quadratic)
--- pyglfw/shader/model_color_light.fs
+++ pyglfw/shader/model_color_light.fs
@@ -8,6 +8,18 @@ struct DirLight {
     vec3 specular;
 };
 
+struct PointLight {
+    vec3 position;
+
+    float constant;
+    float linear;
+    float quadratic;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
 in vec3 ourColor;
 in vec3 fragPos;
 in vec3 fragNormal;
@@ -15,8 +27,10 @@ out vec4 color;
 
 uniform vec3 viewPos;
 uniform DirLight dirLight;
+uniform PointLight pointLight;
 
 vec3 calcDirLight(DirLight light, vec3 normal, vec3 viewDir);
+vec3 calcPointLight(PointLight light, vec3 normal, vec3 viewDir);
 
 void main()
 {
@@ -24,7 +38,10 @@ void main()
     vec3 norm = fragNormal;
     vec3 viewDir = normalize(viewPos - fragPos);
 
+    // Phase 1: Directional lighting
     vec3 result = calcDirLight(dirLight, norm, viewDir);
+    // Phase 2: Point lights
+    result += calcPointLight(pointLight, norm, viewDir);
 
     color = vec4(result, 1.0);
 }
@@ -50,4 +67,36 @@ vec3 calcDirLight(DirLight light, vec3 normal, vec3 viewDir)
     // return ourColor;
     // return viewDir;
     // return normal;
+}
+
+vec3 calcPointLight(PointLight light, vec3 normal, vec3 viewDir)
+{
+    vec3 lightDir = normalize(light.position - fragPos);
+    // Diffuse shading
+    float diff = max(dot(normal, lightDir), 0.0);
+    // Specular shading
+    // vec3 reflectDir = reflect(-lightDir, normal);
+    // float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    // Attenuation
+    float d = length(light.position - fragPos);
+    float attenuation;
+    if (light.constant == 0.0 && light.linear == 0.0 && light.quadratic == 0.0) {
+        attenuation = 1.0;
+    }
+    else {
+        attenuation = 1.0 / (light.constant + light.linear * d + light.quadratic * (d * d));
+    }
+
+    // Combine results
+    // vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
+    // vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
+    // vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
+    // ambient *= attenuation;
+    // diffuse *= attenuation;
+    // specular *= attenuation;
+    // return (ambient + diffuse + specular);
+
+    vec3 diffuse = light.diffuse * diff * ourColor * attenuation;
+    return diffuse;
+    // return light.diffuse * diff * ourColor;
 }
