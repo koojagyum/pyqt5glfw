72f28f9f84970da1fd138ffa61b6e1dc4f4390f2
Marvin <marvin.koo@snowcorp.com>
2019-04-08 11:50:14 +0900

    pyglfw: New property point_size of Model

--- pyglfw/model.py
+++ pyglfw/model.py
@@ -105,6 +105,7 @@ class ColorModel:
                  edges=None, faces=None,
                  color=None, attributes=None,
                  draw_point=True,
+                 point_size=gl_point_size,
                  wireframe=False):
         self.name = name
         self._vertices = None
@@ -114,6 +115,7 @@ class ColorModel:
         self._color_pending = None
         self._attrs_pending = None
         self.draw_point = draw_point
+        self.point_size = point_size
 
         self._vertexobj = None
         self._indexobj_edges = None
@@ -149,7 +151,7 @@ class ColorModel:
 
         with self._vertexobj as vo:
             if self.draw_point:
-                glPointSize(gl_point_size)
+                glPointSize(self.point_size)
                 glDrawArrays(GL_POINTS, 0, vo.vertex_count)
             if self._indexobj_edges is not None:
                 with self._indexobj_edges as ebo:
@@ -385,7 +387,8 @@ class TextureModel:
                  vertices=None,
                  edges=None, faces=None,
                  attributes=None,
-                 material=None):
+                 material=None,
+                 point_size=gl_point_size):
         self.name = name
         self._vertices = None
         self._attrs = {}
@@ -399,6 +402,7 @@ class TextureModel:
         self.vertices = vertices
         self.attrs = attributes
         self.material = material
+        self.point_size = point_size
 
         self._edges = edges
         self._faces = faces
@@ -419,7 +423,7 @@ class TextureModel:
 
         with self.material(program):
             with self._vertexobj as vo:
-                glPointSize(gl_point_size)
+                glPointSize(self.point_size)
                 glDrawArrays(GL_POINTS, 0, vo.vertex_count)
                 if self._indexobj_edges is not None:
                     with self._indexobj_edges as ebo:
